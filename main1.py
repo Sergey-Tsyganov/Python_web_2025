045acc1a5c368262d9aea2f85be19f5701aad91b
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-25 14:24:32 +0300

    swapping

--- main1.py
+++ main1.py
@@ -1,13 +1,9 @@
-hour = 21.59
-if hour > 23:
-    hour = 23
-elif hour < 0:
-    hour = 0
-if 7 > hour >= 22:
-    print('доброй ночи')
-elif hour < 12:
-    print('доброе утро')
-elif hour < 17:
-    print('добрый день')
-else:
-    print('добрый вечер')
+a = 3
+b= 5
+print('a=',a,'b=',b)
+temp = a
+a = b
+b = temp
+print('a=',a,'b=',b)
+a,b =b,a
+print('a=',a,'b=',b)
