c678e84bff5ce1b4d3af758122012de1fd45c028
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-25 15:04:01 +0300

    dlina slova

--- main1.py
+++ main1.py
@@ -1,7 +1,8 @@
-#iterable object
-#len() количество элементов в объекте
+# iterable object
+# len() количество элементов в объекте
 clovo = input('введите слово: ')
-if len(str(clovo))<4:
-    print('слово меньше 4 букв')
+if len(str(clovo)) <= 4:
+    print('слово меньше или равно 4 буквы')
 else:
-    print('слово больше или равно 4 букв')
+    print('Длина слова "'+clovo+'"',len(str(clovo)))
+print('длина - ',clovo)
