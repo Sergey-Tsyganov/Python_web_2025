caeedcf7bb6fda7fcde058e8bd52e8b97c6d20c4
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-25 14:10:00 +0300

    приветствие верное

--- main1.py
+++ main1.py
@@ -3,13 +3,11 @@ if hour > 23:
     hour = 23
 elif hour < 0:
     hour = 0
-if hour < 7:
+if 7 > hour >= 22:
     print('доброй ночи')
 elif hour < 12:
     print('доброе утро')
 elif hour < 17:
     print('добрый день')
-elif hour < 22:
-    print('добрый вечер')
 else:
-    print('доброй ночи')
+    print('добрый вечер')
