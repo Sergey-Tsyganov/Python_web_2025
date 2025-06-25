3b28eb27d6d92462760b394f0f361ffcb6a5d748
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-25 16:23:55 +0300

    homework

--- main1.py
+++ main1.py
@@ -3,8 +3,8 @@ email = "aaa@bbb.ru"
 age = 32
 weight = 34.33
 # способ
-print('Имя: %s, E-mail: %s, Возраст: %d' % (name,email,age))
+print('Имя: %s, E-mail: %s, Возраст: %d' % (name, email, age))
 # 2 способ
-#print('Имя: {}, E-mail: {}, Возраст: {}' .format(name,email.age))
+# print('Имя: {}, E-mail: {}, Возраст: {}' .format(name,email.age))
 # 3 способ
-print(f'Имя: {name}, E-mail: {email},Возраст: {age} , weight{weight:6.3f}')
+print(f'Имя:     {name} \nE-mail:  {email} \nВозраст: {age}  \nweight: {weight:6.2f}')
