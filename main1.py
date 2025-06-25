028c7e6f199353d5a565387cce0d27bbe279fbfc
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-25 14:39:09 +0300

    проба вбок 2

--- main1.py
+++ main1.py
@@ -1,10 +1,13 @@
-a = 3
-b= 5
-print('a=',a,'b=',b)
-temp = a
-a = b
-b = temp
-print('a=',a,'b=',b)
-a,b =b,a
-print('a=',a,'b=',b)
-# комментарий из веба
+hour = 21.59
+if hour > 23:
+    hour = 23
+elif hour < 0:
+    hour = 0
+if 7 > hour >= 22:
+    print('доброй ночи')
+elif hour < 12:
+    print('доброе утро')
+elif hour < 17:
+    print('добрый день')
+else:
+    print('добрый вечер')
