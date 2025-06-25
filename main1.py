77278269b77c820243cafa7b9f5dc510569e7b8c
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-25 16:05:29 +0300

    format

--- main1.py
+++ main1.py
@@ -1,13 +1,16 @@
-hour = 21.59
-if hour > 23:
-    hour = 23
-elif hour < 0:
-    hour = 0
-if 7 > hour >= 22:
-    print('доброй ночи')
-elif hour < 12:
-    print('доброе утро')
-elif hour < 17:
-    print('добрый день')
-else:
-    print('добрый вечер')
+# Формат вывода
+# \ - начало escape sequence ("экран")
+# \n - перевод строки
+# \t - табуляция
+# \x - вывод символа по 2-знакоместам 16-формате (ASCII)
+# \u - вывод символа по 4-знакоместам 16-формате (Unicode)
+# Burned Again Shell - BUSH
+word1 = 'пришел'
+word2 = 'увидел'
+word3 = 'победил'
+word4 = '27\xB0C'  # ASCII
+
+print(word1, word2, word3, end=' -> ', sep=', ')
+print(word4)
+print('Концерт группы \"Кино\"')
+print('Путь к файлу: С:\\Program Files\\bin')
