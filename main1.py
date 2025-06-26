24b42ee8e02b5ed515064f9074acba295fb8c91b
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 11:50:47 +0300

    while loop

--- main1.py
+++ main1.py
@@ -1,15 +1,13 @@
-print('алгоритм поиска решения квдратного уравнения ax^2+bx+c')
-a=float(input('Введите a: '))
-b=float(input('Введите b: '))
-c=float(input('Введите  c: '))
-if a!=0:
-    discr = b**2 - 4 *a*c
-    if discr<0:
-        print('решения нет')
-    elif discr==0:
-        x=-b/(2*a)
-        print(f'решение одно: x =  {x:.2f}')
-    else:
-        print('два решения : \n\t x1= ',-b+discr**0.5/(2*a),'\n\t x2= ',(-b+discr**0.5/(2*a)))
-else:
-    print('По условию а не равно нулю должно быть')
+# while
+# for
+# бинарные операторы += -= /= *=
+count = 0
+while count < 5:
+    #count = count + 1
+
+    print(F'итерация №{count+1}')
+    count += 1
+while count > 0:
+    #count = count + 1
+    count -= 1
+    print(F'итерация №{count+1}')
