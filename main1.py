fdf20645cde13964a8eb531fdf6941c557e9c2cf
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 16:11:23 +0300

    подбор по росту

--- main1.py
+++ main1.py
@@ -1,7 +1,31 @@
-for i in range(1,10):
-    for j in range(1,10):
-        p=i*j
-        print (f'   {p:2}',sep='  ',end='  ')
-    print ('')
+# подбор оп росту 150-180
+#число
+#кандидатов
+#число
+#прошедших
+#среди
+#прошедших:
+#мин
+#рост
+#макс
+#прост
 
+count = 0
+count_cand = 0
+max_h = -float('inf')
+min_h = float('inf')
 
+while True:
+
+    height = int(input('введите рост'))
+    if height == -1:
+        break
+    count += 1
+    if 150 < height < 180:
+        count_cand += 1
+        if height > max_h:
+            max_h = height
+        if height < min_h:
+            min_h = height
+
+print(f'всего {count} кандидатов, прошли отбор {count_cand}, мин рост {min_h}, макс рост {max_h}')
