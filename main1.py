f2d68b5c8b363268b4944203fd46533483b10add
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 14:38:08 +0300

    keyword in

--- main1.py
+++ main1.py
@@ -1,6 +1,5 @@
-counter = 0
-while counter < 100:
-    if counter % 10 == 3:
-        print(counter)
-    counter += 1
-print('Все')
+#ключевое слово in
+
+word = 'поток'
+if 'ток' in word:
+    print('Ок')
