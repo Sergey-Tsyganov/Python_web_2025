f4b70bdab67470f70a6b0ed4040800c45b2e93be
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 15:41:14 +0300

    min max

--- main1.py
+++ main1.py
@@ -1,14 +1,18 @@
-#цикл for
-# for... in ...:
+N = 5
+total = 0
+prod = 1
+min_val = float('inf')
+max_val = -float('inf')
+for _ in range(N):
+    num = int(input('введите целое: '))
+    if num < min_val:
+        min_val = num
+    if num > max_val:
+        max_val = num
+    total += num
+    prod *= num
 
-#word = 'поток'
-#for ch in word:
-#    print(ch)
-
-#rahge (start,stop,step)
-
-for i in range(1,101):
-    if i % 10 == 5:
-        if i == 15:
-            continue
-        print(i)
+print(total, sep='\n')
+print(total / N)
+print(prod)
+print(min_val, max_val)
