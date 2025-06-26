4be1130ed28a5d7b839321b82c67b7b7ff16a904
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 14:35:49 +0300

    while counter

--- main1.py
+++ main1.py
@@ -1,17 +1,6 @@
-# match  - case
-print('Возможные ходы: \n\tL - влево\n\tR -вправо\n\tF -вперед\n\tQ -выход')
-#ch = input('Ваш выбор: ')
-while True:
-    ch = input('Ваш выбор: ')
-    match ch:
-        case 'Q' | 'q':
-            print('Выход')
-            break
-        case 'L' | 'l':
-            print('Свернули налево')
-        case 'R' | 'r':
-            print('Свернули направо')
-        case 'F' | 'f':
-            print('Свернули прямо')
-        case _:
-            print('Выбор неверно')
+counter = 0
+while counter < 100:
+    if counter % 10 == 3:
+        print(counter)
+    counter += 1
+print('Все')
