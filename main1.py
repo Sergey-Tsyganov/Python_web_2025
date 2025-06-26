797d8f13974140e9c94341be56cd2e03f0873c8b
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 12:37:41 +0300

    loop flag

--- main1.py
+++ main1.py
@@ -1,5 +1,14 @@
-# for
-#цикл до ввода пустой строки
-while slovo := input('введите слово ') != '':
-    pass
-print ('пустая строка')
+#
+num = 3
+vars=''
+print('Угадай число')
+flag = True
+while flag:
+    vars= int(input('ваше значение'))
+    if vars==num:
+        print('угадал')
+        flag = not flag
+    else:
+        vars = int(input('введи снова'))
+print('приходи еще')
+
