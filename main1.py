fa3a2675e82ee5c499338b1fdcfcd17432748023
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 14:24:31 +0300

    loop match case

--- main1.py
+++ main1.py
@@ -1,5 +1,17 @@
-h = int(input('введите рост человека: '))
-while not 150 <= h <= 180:
-    print('Не подходит', h)
-    h = int(input('введите рост человека: '))
-print(f'Рост: {h} - отлично')
+# match  - case
+print('Возможные ходы: \n\tL - влево\n\tR -вправо\n\tF -вперед\n\tQ -выход')
+#ch = input('Ваш выбор: ')
+while True:
+    ch = input('Ваш выбор: ')
+    match ch:
+        case 'Q' | 'q':
+            print('Выход')
+            break
+        case 'L' | 'l':
+            print('Свернули налево')
+        case 'R' | 'r':
+            print('Свернули направо')
+        case 'F' | 'f':
+            print('Свернули прямо')
+        case _:
+            print('Выбор неверно')
