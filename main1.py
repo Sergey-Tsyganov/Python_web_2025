f53a415cb9547027df5abfeec36c9605efd64067
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 15:51:26 +0300

    umnozh

--- main1.py
+++ main1.py
@@ -1,18 +1,7 @@
-N = 5
-total = 0
-prod = 1
-min_val = float('inf')
-max_val = -float('inf')
-for _ in range(N):
-    num = int(input('введите целое: '))
-    if num < min_val:
-        min_val = num
-    if num > max_val:
-        max_val = num
-    total += num
-    prod *= num
+for i in range(1,10):
+    for j in range(1,10):
+        p=i*j
+        print (f'   {p:2}',sep='  ',end='  ')
+    print ('')
 
-print(total, sep='\n')
-print(total / N)
-print(prod)
-print(min_val, max_val)
+
