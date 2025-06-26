4019ba6c42ad427dc38a2d292be3a2d6ceea3a35
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 12:52:34 +0300

    loop continue

--- main1.py
+++ main1.py
@@ -1,14 +1,8 @@
-#
-num = 3
-vars=''
-print('Угадай число')
-flag = True
-while flag:
-    vars= int(input('ваше значение'))
-    if vars==num:
-        print('угадал')
-        flag = not flag
-    else:
-        vars = int(input('введи снова'))
-print('приходи еще')
 
+count = 0
+while count <5:
+    count += 1
+    if count ==3:
+
+        continue
+    print('Итерация №',count)
