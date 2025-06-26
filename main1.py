9429492392573550124056916ddb1d836e9bd773
Sergey Tsyganov <tsysn1@gmail.com>
2025-06-26 10:31:05 +0300

    homework

--- main1.py
+++ main1.py
@@ -1,10 +1,10 @@
-name = 'Igor'
-email = "aaa@bbb.ru"
-age = 32
-weight = 34.33
-# способ
-print('Имя: %s, E-mail: %s, Возраст: %d' % (name,email,age))
-# 2 способ
-#print('Имя: {}, E-mail: {}, Возраст: {}' .format(name,email.age))
-# 3 способ
-print(f'Имя: {name}, E-mail: {email},Возраст: {age} , weight{weight:6.3f}')
+print('алгоритм поиска фальшивой монеты')
+a=float(input('Введите вес первой монеты: '))
+b=float(input('Введите вес второй монеты: '))
+c=float(input('Введите вес третьей монеты: '))
+if a==b:
+    print('Фальшивая монета С')
+elif a==c:
+    print('Фальшивая монета В')
+else:
+    print('Фальшивая монета A')
