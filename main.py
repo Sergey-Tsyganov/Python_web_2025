d0889dd6f90fcc9f4d959638d6885a7bd8c01dd0
Sergey-Tsyganov <s_tsyganov@hotmail.com>
2025-07-16 11:48:09 +0300

    sample page

--- main.py
+++ main.py
@@ -1,2 +1,44 @@
-temp = 36.7
-print('Температура', temp,' слишком высокая, это почти ', temp//1+ 1, '.')
+# введение во Flask
+#MVC - model view controller
+from flask import Flask, url_for
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return 'Привет ,Flask'
+
+@app.route('/about')
+def about():
+#    print('Вызвана функция about')
+    return 'О нас'
+
+@app.route('/countdown')
+def cd():
+    lst = [str(x) for x in reversed(range(10))]
+    lst.append('Полетели!!!')
+    return '<br>'.join(lst)
+
+@app.route('/image')
+def show_image():
+    return f'<img src="{url_for('static',filename='images/vinny.jpg')}">'
+
+
+@app.route('/sample-page')
+def sample_page():
+    return f"""
+        <!DOCTYPE html>
+        <html lang="ru">
+        <head>
+            <meta charset="UTF-8">
+            <title>Картинка Виннни-Пуха</title>
+        </head>
+        <body>
+            <img src="{url_for('static',filename='/images/vinny.jpg')}" alt="Vinny">
+        </body>
+        </html>
+        """
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
--- old/main1.py
+++ old/main1.py
@@ -2,6 +2,8 @@
 # from PIL.SpiderImagePlugin import iforms
 # from openpyxl import load_workbook
 import csv
+from tkinter import image_names
+#from encodings.punycode import selective_find
 from zipfile import ZipFile
 
 import schedule
@@ -914,15 +916,197 @@ from lib import Circle, Square
 # для чтения
 # load() из файла
 # loads() - читает строковое представление
-import json
-with open('dogs.json','rt') as d:
-    #data = json.load(d)
-    temp = d.read()
-    data = json.loads(temp)
-    print(data)
-    print (data)
-for k,v in data.items():
-    if type(v) == list:
-        print(f'{k}: {', '.join(v) }')
-    else:
-        print(f'{k},{v}')
+# import json
+# # with open('dogs.json','rt') as d:
+# #     #data = json.load(d)
+# #     temp = d.read()
+# #     data = json.loads(temp)
+# #     print(data)
+# #     print (data)
+# # for i in data():
+# # for k,v in data.items():
+# #     if type(v) == list:
+# #         print(f'{k}: {', '.join(v) }')
+# #     else:
+# #         print(f'{k},{v}')
+#
+# d = {
+#     'Ананас': 300,
+#     'Банан': 400,
+#     'Яблоко': 120,
+#     'Груща': 100
+# }
+#
+# # with open('fruits.json','w', encoding = 'utf-8') as f:
+# #     json.dump(d,f, indent = 4)
+# #вывод в виде строки
+# data =json.dumps(d)
+# print(d)
+
+
+# базы данных
+"""
+Импорт бибилиотеке  sqlite 3
+Подключаемся к БД
+Назначить  курсор
+Работаем
+подтвердить изменения commit
+Закрываем БД
+"""
+import sqlite3
+import csv
+# connection = sqlite3.connect('db/movies.sqlite')
+# # cursor = connection.cursor()
+# # with open('people.csv', 'r', encoding = 'utf-8') as f:
+# #     reader = csv.reader(f, delimiter=',')
+# #     next(reader) #пропустить первую строку
+# #
+# #
+# # for name, age  in reader:
+# #     cursor.execute(
+# #     """
+# #         insert into users(name,age) values(?,?)
+# #          """,(name,int(age)) )
+# #
+# # connection.commit()
+# # connection.close()
+#
+# Базы данных (запись)
+# Базы данных (запись)
+# """
+# 1. Импорт библиотеки sqlite3
+# 2. Подключаемся к БД
+# 3. Назначить "курсор"
+# 4. Работаем с БД (запросы и ответы)
+# 5. Подтвердить изменение (commit)
+# 6. Отключаемся от БД
+# """
+# import sqlite3
+#
+#
+# class Crud:
+#     def __init__(self, db_path):
+#         self._conn = sqlite3.connect(db_path)
+#         self._cur = self._conn.cursor()
+#
+#     def create(self, table_name, name, age):
+#         self._cur.execute(
+#             f"""
+#             INSERT INTO {table_name}(name, age)
+#              VALUES(?, ?)
+#              """, (name, int(age))
+#         )
+#         self._conn.commit()
+#
+#     def read(self, table_name):
+#         res = self._cur.execute(
+#             f'SELECT * FROM {table_name}'
+#         ).fetchall()
+#
+#         for num, name, age in res:
+#             print(num, name, age)
+#
+#     def update(self, table_name, id_num, name=None, age=None):
+#         query = f'UPDATE {table_name} SET name="{name}", age={age} WHERE id={id_num}'
+#         # print(query)
+#         self._cur.execute(
+#             query
+#         )
+#         self._conn.commit()
+#
+#     def delete(self, id_num, table_name):
+#         self._cur.execute(
+#             f'DELETE FROM {table_name} WHERE id={id_num}'
+#         )
+#         self._conn.commit()
+#
+#     # method override (переопределяем метод уничтожения объекта)
+#     def __del__(self):
+#         self._cur.close()  # отключаем курсор
+#         self._conn.close()  # отключаемся от БД
+#
+#
+# db = Crud('db/movies.sqlite')
+# # db.delete(3, 'users')
+# # db.create('users', 'Дмитрий', 18)
+# db.update('users', 6, 'Евгений', 19)
+# db.read('users')
+#
+# import requests
+# from PIL import Image
+# import io
+# API_KEY = '86f96d1e82a270046b1028edfeea3eda'
+# URL= 'https://api.openweathermap.org/data/2.5/weather'
+# CITY = 'Санкт-Петербург'
+#
+# params ={
+#     'q':CITY,
+#     'appid': API_KEY, 'units': 'metric',
+#     'lang': 'ru'}
+# responce = requests.get(URL,params=params)
+#
+# result = responce.json()
+# print(result)
+# print(result['weather'])
+# weather = result['weather'][0]['description']
+# temperature = result['main']['temp']
+# humidity = result['main']['humidity']
+# wind =result['wind']['speed']
+#
+# print(f' сегодня  городе {CITY} {weather}')
+# print(f' температура: {temperature:.1f}')
+# data = result['coord']
+# ll= f'{data['lon']},{data['lat']}'
+# print (ll)
+# link=f'https://static-maps.yandex.ru/1.x/?ll={ll}&spn=0.0025,0.0025&l=map&pt={ll},pm2dgl'
+# image = requests.get(link).content
+# if image:
+#     im = Image.open(io.BytesIO(image)).convert('RGB')
+#     im.save('map.jpg')
+
+
+#декораторы
+
+# def upper_print(old_func):
+#     def new_func(*args,**kwargs):
+#         case = kwargs.pop('case',None)
+#         if case == 'U':
+#             args = [str(arg).upper() for arg in args]
+#         elif case=='L':
+#             args= [str(arg).lower() for arg in args]
+#         return old_func(*args_up_case,**kwargs)
+#     return new_func
+#
+#
+# new_print = upper_print((print))
+#
+# new_print(('Привет, Пока'))
+# g=4
+# def outer():
+#     x=5
+#
+#     def inner():
+#         nonlocal x
+#         print('Nonlocal x = ',x)
+#         x=10
+#
+#     inner()
+#     print('Ntw x =', x)
+#
+# outer()
+
+def logger(func):
+    counter = 0
+    def decorated_function(*args,**kwargs):
+        nonlocal counter
+        counter +=1
+        print(counter, '->', 'Аргументы', args, 'Исенованные аргументы' ,kwargs)
+
+        result  = func(*args,**kwsrgs)
+        print('_________','Результтат: ', result)
+
+        return decorated_function
+
+        def make_burger(mael,onion=False, TOmato = False):
+            print('Булочка')
+            
